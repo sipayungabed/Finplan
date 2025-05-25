2ca2f64b1ce77815e1c18816554bf42504dfa345
abedsipayung <53556147+sipayungabed@users.noreply.github.com>
2025-05-25 18:14:35 +0700

    chore: :tada: bootstrap Spring Boot project for finplan api app

---
+++ api/src/main/java/com/abednego/finplan/FinplanApiApplication.java
@@ -0,0 +1,13 @@
+package com.abednego.finplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinplanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinplanApiApplication.class, args);
+	}
+
+}
